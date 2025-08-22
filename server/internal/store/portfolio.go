ef9abbe27a6a345355de8bf24e55e5b225577343
Kasyap1234 <kasyap3103@gmail.com>
2025-08-22 14:37:26 +0530

    add create portfolio

--- server/internal/store/portfolio.go
+++ server/internal/store/portfolio.go
@@ -6,27 +6,34 @@ import (
 	"github.com/google/uuid"
 	"github.com/jackc/pgx/v5/pgtype"
 	"github.com/kasyap1234/portfolio/server/internal/db"
-	"github.com/kasyap1234/portfolio/server/internal/db/portfolio"
 	"github.com/kasyap1234/portfolio/server/internal/models"
 )
 
 type PortfolioStore interface {
-CreatePortfolio(ctx context.Context,user_id uuid.UUID,name string,invested_value string , current_value string)(*models.Portfolio,error)
+	CreatePortfolio(ctx context.Context, user_id uuid.UUID, name string, invested_value string, current_value string) (*models.Portfolio, error)
 }
 
-type portfolioStore  struct {
+type portfolioStore struct {
 	q *db.Queries
 }
 
-func(p*portfolioStore)CreatePortfolio(ctx context.Context,user_id uuid.UUID,name,invested_value,current_value string)(*models.Portfolio,error){
+func (p *portfolioStore) CreatePortfolio(ctx context.Context, user_id uuid.UUID, name, invested_value, current_value string) (*models.Portfolio, error) {
 	args := db.CreatePortfolioParams{
-		UserID: user_id,
-		Name: name,
+		UserID:        user_id,
+		Name:          name,
 		InvestedValue: pgtype.Text{String: invested_value},
-		CurrentValue: pgtype.Text{String: current_value},
-
+		CurrentValue:  pgtype.Text{String: current_value},
 	}
-	folio,err :=p.q.CreatePortfolio(ctx,args)
-	 
+	folio, err := p.q.CreatePortfolio(ctx, args)
+	portfolio := &models.Portfolio{
+		ID:            folio.ID,
+		UserID:        folio.UserID,
+		Name:          folio.Name,
+		InvestedValue: folio.InvestedValue.String,
+		CurrentValue:  folio.CurrentValue.String,
+		CreatedAt:     folio.CreatedAt.Time,
+		UpdatedAt:     folio.UpdatedAt.Time,
+	}
+	return portfolio, err
 
 }
